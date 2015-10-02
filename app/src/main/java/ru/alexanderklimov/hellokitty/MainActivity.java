acf5b631d66874f91db5537577fa774609a84684
Alexander Klimoff <alexander.klimoff@gmail.com>
2015-10-02 12:56:54 +0300

    Исправлен код под новый шаблон для Android Studio 1.4

--- app/src/main/java/ru/alexanderklimov/hellokitty/MainActivity.java
+++ app/src/main/java/ru/alexanderklimov/hellokitty/MainActivity.java
@@ -3,10 +3,8 @@
 
 package ru.alexanderklimov.hellokitty;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,28 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
         mHelloTextView = (TextView) findViewById(R.id.textView);
         mNameEditText = (EditText) findViewById(R.id.editText);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void onClick(View view) {
